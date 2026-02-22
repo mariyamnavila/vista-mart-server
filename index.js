6d921da5d9e56c5966c4006e68b7baad2e02cac9
mariyamnavila <bibimariyamnavila@gmail.com>
2026-02-22 20:24:45 +0600

    added get for product

--- index.js
+++ index.js
@@ -25,6 +25,70 @@ async function run() {
     try {
         await client.connect();
 
+        const db = client.db('vista-mart-db')
+        const productCollection = db.collection('products')
+
+        // GET /products?page=1&limit=8&search=pro&brand=Apple&minPrice=300&sort=newest
+        app.get("/products", async (req, res) => {
+            try {
+                const {
+                    page = 1,
+                    limit = 10,
+                    search,
+                    brand,
+                    category,
+                    minPrice,
+                    maxPrice,
+                    sort,
+                } = req.query;
+
+                const query = {};
+
+                // 🔎 Search by name
+                if (search) {
+                    query.name = { $regex: search, $options: "i" };
+                }
+
+                // 🎯 Filter
+                if (brand) query.brand = brand;
+                if (category) query.category = category;
+
+                if (minPrice && maxPrice) {
+                    query.price = {
+                        $gte: Number(minPrice),
+                        $lte: Number(maxPrice),
+                    };
+                }
+
+                // 🔃 Sorting
+                let sortOption = {};
+                if (sort === "priceLow") sortOption.price = 1;
+                if (sort === "priceHigh") sortOption.price = -1;
+                if (sort === "newest") sortOption.createdAt = -1;
+
+                const skip = (Number(page) - 1) * Number(limit);
+
+                const products = await productCollection
+                    .find(query)
+                    .sort(sortOption)
+                    .skip(skip)
+                    .limit(Number(limit))
+                    .toArray();
+
+                const total = await productCollection.countDocuments(query);
+
+                res.send({
+                    products,
+                    totalPages: Math.ceil(total / limit),
+                    currentPage: Number(page),
+                    totalProducts: total,
+                });
+
+            } catch (error) {
+                res.status(500).send({ message: error.message });
+            }
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
