f1037aae740b7b1d53956b09c596847a208a8b56
mariyamnavila <bibimariyamnavila@gmail.com>
2026-02-24 00:23:57 +0600

    made some changes

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         const db = client.db('vista-mart-db')
         const productCollection = db.collection('products')
@@ -103,8 +103,8 @@ async function run() {
             }
         });
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
